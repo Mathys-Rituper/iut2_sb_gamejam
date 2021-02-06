26a1479ff6939c6bc1d22287750cdd01ddca6829
Mathys Rituper <mathys.rituper@etu.univ-grenoble-alpes.fr>
2021-02-06 20:01:55 +0100

    Cleanup

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Game Jam IUT2 Informatique Grenoble 2021
 
-##Game : blablabla NoName
+## Game : blablabla NoName
 
-###Team : Mathys Rituper, Mael Veyrat, Jonathan Charlassier, Lucas Arsicaud 
+### Team : Mathys Rituper, Mael Veyrat, Jonathan Charlassier, Lucas Arsicaud 
 ###
 #### Required packages
 - pygame
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
 
+#Initialisation de pygame
 pygame.init()
-pygame.display.set_caption("Game Jam 2021")
+
+#Initialisation de la fenêtre
 screen = pygame.display.set_mode((1024, 768))
+pygame.display.set_caption("Game Jam 2021")
+
+
 screen.blit(pygame.image.load("assets/default.jpg"), (0, 0))
+
+pygame.display.flip()
 
 ouvert = True
 
 while ouvert:
     for event in pygame.event.get():
-        if event.type == pygame.quit():
+        if event.type == pygame.QUIT:
             ouvert = False
 
 pygame.quit()
