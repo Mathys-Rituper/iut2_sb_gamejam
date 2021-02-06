354bc6248a4900dbb4a027a777edca93f2196fd1
Mathys Rituper <mathys.rituper@etu.univ-grenoble-alpes.fr>
2021-02-06 19:44:39 +0100

    Cleanup

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_caption("Game Jam 2021")
-pygame.display.set_mode((1024, 768))
+pygame.display.set_caption("Game Jam 2021")
+screen = pygame.display.set_mode((1024, 768))
+screen.blit(pygame.image.load("assets/default.jpg"), (0, 0))
 
+ouvert = True
+
+while ouvert:
+    for event in pygame.event.get():
+        if event.type == pygame.quit():
+            ouvert = False
 
 pygame.quit()
