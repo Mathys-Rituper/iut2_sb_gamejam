1218cb4eaee4a4ec15d2f15b5556c4e0c0031c03
Mathys Rituper <mathys.rituper@etu.univ-grenoble-alpes.fr>
2021-02-06 19:26:21 +0100

    Pygame requirements

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pygame
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+pygame.init()
+screen = pygame.display.set_caption("Game Jam 2021")
+pygame.display.set_mode((1024, 768))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-for i in range(3, 5):
-    print(i)
+pygame.quit()
