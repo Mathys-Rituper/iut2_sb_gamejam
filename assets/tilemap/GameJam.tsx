01dcfa348ec96ca0b86ecf8a5c2e57b115f301b0
Mael VEYRAT <mael.veyrat@etu.univ-grenoble-alpes.fr>
2021-02-09 14:34:51 +0100

    Map au propre

--- assets/tilemap/GameJam.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="GameJam" tilewidth="32" tileheight="32" tilecount="15744" columns="16">
- <image source="terrain-map-v7.png" width="512" height="31488"/>
-</tileset>
