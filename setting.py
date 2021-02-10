e1e1ea0f1531592a9cf37404f828bd4ef3579e00
Mael VEYRAT <mael.veyrat@etu.univ-grenoble-alpes.fr>
2021-02-10 12:50:57 +0100

    Map a jour

--- setting.py
+++ setting.py
@@ -1,8 +1,8 @@
 #définition des données pour le tile mapping
 
 WIDTH_TILE = 32
-NB_TILE_X = 120
+NB_TILE_X = 164
 HEIGHT_TILE = 32
-NB_TILE_Y = 75
+NB_TILE_Y = 99
 SCREEN_HEIGHT = 1080
 SCREEN_WIDTH  = 768
