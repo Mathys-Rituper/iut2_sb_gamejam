ee5bf7910630dcc3a01341354015b6aa90e00dd2
Mael VEYRAT <mael.veyrat@etu.univ-grenoble-alpes.fr>
2021-02-11 20:14:16 +0100

    weapons

--- weapons/weapons.py
+++ weapons/weapons.py
@@ -17,26 +17,98 @@ class Pistolet(pygame.sprite.Sprite):
         if (self.player.cd()/self.player.attack_speed) > self.cooldown:
             direction = self.player.current_orientation
             if direction == "right":
-                x = self.player.rect.center[0] + 16
+                x = self.player.rect.center[0]
                 y = self.player.rect.center[1]
-                dx = self.player.velocity + random.randint(1, 3)
+                dx = self.vitesse_projectile + random.randint(1, 3)
                 dy = ((random.random() - 0.5) * 2)
             elif direction == "left":
-                x = self.player.rect.center[0] - 16
+                x = self.player.rect.center[0]
                 y = self.player.rect.center[1]
-                dx = -self.player.velocity + random.randint(-3, -1)
+                dx = -self.vitesse_projectile + random.randint(-3, -1)
                 dy = ((random.random() - 0.5) * 2)
             elif direction == "up":
                 x = self.player.rect.center[0]
-                y = self.player.rect.center[1] - 16
-                dy = -self.player.velocity + random.randint(-3, -1)
+                y = self.player.rect.center[1]
+                dy = -self.vitesse_projectile + random.randint(-3, -1)
                 dx = ((random.random() - 0.5) * 2)
             else:
                 x = self.player.rect.center[0]
-                y = self.player.rect.center[1] + 16
-                dy = self.player.velocity + random.randint(1, 3)
+                y = self.player.rect.center[1]
+                dy = self.vitesse_projectile + random.randint(1, 3)
                 dx = ((random.random() - 0.5) * 2)
             self.player.game.projectiles.add(Projectile(self.player.game, x, y, dx, dy, self.damage, self.ttl, 30))
+            self.player.reset_cd()
+
+class Mitrailleuse(pygame.sprite.Sprite):
+
+    def __init__(self, player):
+        super().__init__()
+        self.player = player
+        self.cooldown = 0.1
+        self.vitesse_projectile = 7
+        self.damage = 2
+        self.ttl = 45
+
+    def fire(self):
+        if (self.player.cd()/self.player.attack_speed) > self.cooldown:
+            direction = self.player.current_orientation
+            if direction == "right":
+                x = self.player.rect.center[0]
+                y = self.player.rect.center[1]
+                dx = self.vitesse_projectile + random.randint(1, 3)
+                dy = ((random.random() - 0.5) * 2)
+            elif direction == "left":
+                x = self.player.rect.center[0]
+                y = self.player.rect.center[1]
+                dx = -self.vitesse_projectile + random.randint(-3, -1)
+                dy = ((random.random() - 0.5) * 2)
+            elif direction == "up":
+                x = self.player.rect.center[0]
+                y = self.player.rect.center[1]
+                dy = -self.vitesse_projectile + random.randint(-3, -1)
+                dx = ((random.random() - 0.5) * 2)
+            else:
+                x = self.player.rect.center[0]
+                y = self.player.rect.center[1]
+                dy = self.vitesse_projectile + random.randint(1, 3)
+                dx = ((random.random() - 0.5) * 2)
+            self.player.game.projectiles.add(Projectile(self.player.game, x, y, dx, dy, self.damage, self.ttl, 1))
+            self.player.reset_cd()
+class Snipe(pygame.sprite.Sprite):
+
+    def __init__(self, player):
+        super().__init__()
+        self.player = player
+        self.cooldown = 1
+        self.vitesse_projectile = 20
+        self.damage = 10
+        self.ttl = 100
+
+
+    def fire(self):
+        if (self.player.cd()/self.player.attack_speed) > self.cooldown:
+            direction = self.player.current_orientation
+            if direction == "right":
+                x = self.player.rect.center[0]
+                y = self.player.rect.center[1]
+                dx = self.vitesse_projectile
+                dy = 0
+            elif direction == "left":
+                x = self.player.rect.center[0]
+                y = self.player.rect.center[1]
+                dx = -self.vitesse_projectile
+                dy = 0
+            elif direction == "up":
+                x = self.player.rect.center[0]
+                y = self.player.rect.center[1]
+                dy = -self.vitesse_projectile
+                dx = 0
+            else:
+                x = self.player.rect.center[0]
+                y = self.player.rect.center[1]
+                dy = self.vitesse_projectile
+                dx = 0
+            self.player.game.projectiles.add(Projectile(self.player.game, x, y, dx, dy, self.damage, self.ttl, 1000))
             self.player.reset_cd()
 
 class Pompe(pygame.sprite.Sprite):
@@ -54,36 +126,36 @@ class Pompe(pygame.sprite.Sprite):
         if (self.player.cd()/self.player.attack_speed) > self.cooldown:
             direction = self.player.current_orientation
             if direction == "right":
-                centre = [self.player.rect.center[0] + 16,
+                centre = [self.player.rect.center[0] ,
                           self.player.rect.center[1]]  # centre de la ligne sur laquelle les balles sont ajoutées
                 for i in range(-3, 4, 1):
                     if i != 0:
                         x = centre[0]
                         y = centre[1] + ((i + random.randint(-3, 3)) * 3)
-                        dx = self.player.velocity + random.randint(3, 5)
+                        dx = self.vitesse_projectile + random.randint(3, 5)
                         dy = (i + random.randint(-3, 3)) * self.spread_factor
                         self.player.game.projectiles.add(Projectile(self.player.game, x, y, dx, dy, self.damage, self.ttl, 1))
 
             elif direction == "left":
-                centre = [self.player.rect.center[0] - 16,
+                centre = [self.player.rect.center[0] ,
                           self.player.rect.center[1]]  # centre de la ligne sur laquelle les balles sont ajoutées
                 for i in range(-3, 4, 1):
                     if i != 0:
                         x = centre[0]
                         y = centre[1] + ((i + random.randint(-3, 3)) * 3)
-                        dx = - self.player.velocity + random.randint(-5,-3)
+                        dx = -self.vitesse_projectile + random.randint(-5,-3)
                         dy = (i + random.randint(-3, 3)) * self.spread_factor
                         self.player.game.projectiles.add(Projectile(self.player.game, x, y, dx, dy, self.damage, self.ttl, 1))
 
             elif direction == "up":
 
                 centre = [self.player.rect.center[0],
-                          self.player.rect.center[1] - 16]  # centre de la ligne sur laquelle les balles sont ajoutées
+                          self.player.rect.center[1] ]  # centre de la ligne sur laquelle les balles sont ajoutées
                 for i in range(-3, 4, 1):
                     if i != 0:
                         x = centre[0] + ((i + random.randint(-3, 3)) * 3)
                         y = centre[1]
-                        dy = - self.player.velocity + random.randint(-5, -3)
+                        dy = - self.vitesse_projectile + random.randint(-5, -3)
                         dx = (i + random.randint(-3, 3)) * self.spread_factor
 
                         self.player.game.projectiles.add(Projectile(self.player.game, x, y, dx, dy, self.damage, self.ttl, 1))
@@ -91,12 +163,12 @@ class Pompe(pygame.sprite.Sprite):
             else:
 
                 centre = [self.player.rect.center[0],
-                          self.player.rect.center[1] + 16]  # centre de la ligne sur laquelle les balles sont ajoutées
+                          self.player.rect.center[1] ]  # centre de la ligne sur laquelle les balles sont ajoutées
                 for i in range(-3, 4, 1):
                     if i != 0:
                         x = centre[0] + ((i + random.randint(-3, 3)) * 3)
                         y = centre[1]
-                        dy = self.player.velocity + random.randint(3, 5)
+                        dy = self.vitesse_projectile + random.randint(3, 5)
                         dx = (i + random.randint(-3, 3)) * self.spread_factor
 
                         self.player.game.projectiles.add(Projectile(self.player.game, x, y, dx, dy, self.damage, self.ttl, 1))
